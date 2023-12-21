8916268488008fa7ca8d915152994adeaee705f3
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:54:11 +0100

    [~] Add the set_largest function -> replace in the buffer the largest found square by 'x'

--- settingup.c
+++ settingup.c
@@ -16,3 +16,17 @@ static int my_min(int a, int b, int c)
     return c;
 }
 
+static int set_largest(char *buffer, int *tab, int dims[2])
+{
+    int max = 0;
+    int max_buf = 0;
+
+    for (int i = 1; i < dims[0] * dims[1]; i++)
+        (tab[i] > tab[max]) && (max = i);
+    max_buf = max +  max / dims[0];
+    for (int i = 0; i < tab[max]; i++)
+        for (int j = 0; j < tab[max]; j++)
+            buffer[max_buf - i * (dims[0] + 1) - j] = 'x';
+    return 0;
+}
+
