1b60790bdf9aa29cbf7f9f926c9eee43f2c1ffcf
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:54:00 +0100

    [+] Add the include folder with the my.h header

---
+++ include/my.h
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#ifndef MY_H
+    #define MY_H
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <time.h>
+    #include <limits.h>
+    #include <stdarg.h>
+    #include <errno.h>
+    #include <fcntl.h>
+    #include <math.h>
+    #include <stdlib.h>
+
+    #include <criterion/criterion.h>
+    #include <criterion/redirect.h>
+
+    #include <SFML/Graphics.h>
+    #include <SFML/Audio.h>
+    #include <SFML/Config.h>
+    #include <SFML/GPUPreference.h>
+    #include <SFML/Network.h>
+    #include <SFML/OpenGL.h>
+    #include <SFML/System.h>
+    #include <SFML/Window.h>
+
+size_t my_strlen(char const *);
+int my_getnbr(char const *);
+
+#endif
