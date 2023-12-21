85e70a04382dc6c9862f1255cd3cab063532a836
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:54:12 +0100

    [~] Add the fill_tab function -> create and fill a array of int that represent the bottom right corner of largest possible square with this corner

--- settingup.c
+++ settingup.c
@@ -30,3 +30,25 @@ static int set_largest(char *buffer, int *tab, int dims[2])
     return 0;
 }
 
+static int fill_tab(int xy[2], int dims[2], char *buf, int *tab)
+{
+    int n = xy[1] * dims[0] + xy[0];
+
+    if (xy[0] < 0 || xy[1] < 0 || xy[0] >= dims[0] || xy[1] >= dims[1])
+        return 0;
+    if (tab[n] != -1)
+        return tab[n];
+    if (buf[n + xy[1]] == 'o') {
+        tab[n] = 0;
+        fill_tab((int[2]){xy[0] + 1, xy[1] + 1}, dims, buf, tab);
+        fill_tab((int[2]){xy[0] - 1, xy[1]}, dims, buf, tab);
+        fill_tab((int[2]){xy[0], xy[1] - 1}, dims, buf, tab);
+        return tab[n];
+    }
+    tab[n] = my_min(fill_tab((int[2]){xy[0] - 1, xy[1]}, dims, buf, tab),
+        fill_tab((int[2]){xy[0] - 1, xy[1] - 1}, dims, buf, tab),
+        fill_tab((int[2]){xy[0], xy[1] - 1}, dims, buf, tab)) + 1;
+    fill_tab((int[2]){xy[0] + 1, xy[1] + 1}, dims, buf, tab);
+    return tab[n];
+}
+
